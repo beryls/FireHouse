87c0745f47c5dce52f7cc25488e9c93da2c23320
Beryl <beryl.schragger@gmail.com>
2013-08-19 09:37:08 -0400

    First commit - scaffolded fires and firefighters, added examples.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FireHouse::Application.config.secret_token = '37f3f4955a688e504ef205758191c9738d58e2912ef60a45e5af6eb5cffd06cba67924229d2ac054a943cf43e592c3d34028e0c2aedbdb4bfac49a662a643580'
