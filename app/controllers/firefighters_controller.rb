b3b5cc49a6fd0a6c3e11dea6400adb16828b8cfa
Beryl <beryl.schragger@gmail.com>
2013-08-19 16:42:48 -0400

    Added expirations for firefighters index and show.

--- app/controllers/firefighters_controller.rb
+++ app/controllers/firefighters_controller.rb
@@ -3,7 +3,7 @@ class FirefightersController < ApplicationController
   # GET /firefighters.json
   def index
     @firefighters = Firefighter.all
-
+    expires_in 3.minutes, public: true
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @firefighters }
@@ -14,7 +14,7 @@ class FirefightersController < ApplicationController
   # GET /firefighters/1.json
   def show
     @firefighter = Firefighter.find(params[:id])
-
+    expires_in 3.minutes, public: true
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @firefighter }
